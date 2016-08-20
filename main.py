6cae0e691655adec41313361a8198238b00a82f1
Pilar Huidobro <huidobropilar@gmail.com>
2016-08-20 15:30:29 +0200

    Stuff

--- main.py
+++ main.py
@@ -42,7 +42,18 @@ def post_webhook():
     return "ok", 200
 
 
-def reply(recipient_id, message_text):
+def rules(recipient_id, message_text):
+    rules = {
+        "Hello": "World",
+        "Foo": "Bar"
+    }
+
+    if message_text in rules:
+        reply(recipient_id, rules[message_text])
+
+    else:
+        reply(recipient_id, "You have to write something I understand ;)")
+        
     params = {
         "access_token": access_token
     }
